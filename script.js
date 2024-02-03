a7ed3368e61dffae2f6e032bf75e755d165e5032
jinss5 <jinss1145@gmail.com>
2024-02-02 23:00:05 -0600

    modified all css file to be in one file

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function increment() {
   document.getElementById("result").innerHTML = "Today's count is " + count;
 }
 
-let page_style = localStorage.getItem("page_stylesheet_name") || "home.css";
+let page_style = localStorage.getItem("page_stylesheet_name") || "style1.css";
 
 function load_style() {
   document.getElementById("style_link").setAttribute("href", page_style);
 }
 
 document.getElementById("page_style").addEventListener("click", function () {
-  page_style = page_style === "home.css" ? "home2.css" : "home.css";
+  page_style = page_style === "style1.css" ? "style2.css" : "style1.css";
 
   localStorage.setItem("page_stylesheet_name", page_style);
 
